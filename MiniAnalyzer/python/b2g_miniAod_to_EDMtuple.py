4054466dfb6fedf717ed15dfbf21c176ce69ddc8
Brandon <bchiarito@gmail.com>
2014-11-25 10:34:00 -0600

    second commit

--- MiniAnalyzer/python/b2g_miniAod_to_EDMtuple.py
+++ MiniAnalyzer/python/b2g_miniAod_to_EDMtuple.py
@@ -2,18 +2,74 @@ import FWCore.ParameterSet.Config as cms
 ###############################################
 # Test V1
 ###############################################
-testFile =  'file:./miniaod_plus.root'
+testFile =  'file:/eos/uscms/store/user/bchiari1/miniaod/ver1/T1tttt_2J_mGo1300_mStop300_mCh285_mChi280_pythia8-23bodydec.MINIAODSIM.00.root'
 ###############################################
 
-process = cms.Process("B2G") # <-- Start Process here
-
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process = cms.Process("B2G")
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(testFile),
-    noEventSort = cms.untracked.bool(True),
-    duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
-			   )
+	fileNames = cms.untracked.vstring(testFile)
+)
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+process.out = cms.OutputModule("PoolOutputModule",
+		   fileName = cms.untracked.string("ntuple.root"),
+		   SelectEvents   = cms.untracked.PSet( SelectEvents = cms.vstring('p')),
+		   outputCommands = cms.untracked.vstring('drop *','keep *_b2g_*_*'))
+process.outpath = cms.EndPath(process.out)
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.out.dropMetaData = cms.untracked.string("DROPPED")
+
+from RecoJets.JetProducers.ak5PFJetsPruned_cfi import ak5PFJetsPruned
+from RecoJets.JetProducers.ak5PFJets_cfi import ak5PFJets
+from RecoJets.JetProducers.nJettinessAdder_cfi import Njettiness
+from RecoJets.JetProducers.caTopTaggers_cff import cmsTopTagPFJetsCHS
+from RecoJets.JetProducers.caTopTaggers_cff import caTopTagInfos
+
+process.chs = cms.EDFilter("CandPtrSelector", src = cms.InputTag("packedPFCandidates"), cut = cms.string("fromPV()>0"))
+process.ca8PFJetsCHS = ak5PFJets.clone(
+					src = 'chs',
+					rParam = cms.double(0.8),
+					jetPtMin = cms.double(3.0),
+					jetAlgorithm = cms.string("CambridgeAachen"))
+process.ca8PFJetsCHSPruned = ak5PFJets.clone(
+					src = 'chs',
+					rParam = cms.double(0.8),
+					jetPtMin = cms.double(3.0),
+					jetAlgorithm = cms.string("CambridgeAachen"),
+					usePruning = cms.bool(True),
+					useExplicitGhosts = cms.bool(True),
+					jetCollInstanceName = cms.string("SubJets"),
+					writeCompound = cms.bool(True),
+					nFilt = cms.int32(2),
+					zcut = cms.double(0.1),
+					rcut_factor = cms.double(0.5))
+
+process.cmsTopTagPFJetsCHS = cmsTopTagPFJetsCHS.clone(src = cms.InputTag('chs'))
+process.caTopTagInfos = caTopTagInfos.clone(src = cms.InputTag("cmsTopTagPFJetsCHS"))
+
+process.selectedca8PFJetsCHS = cms.EDFilter('PFJetSelector',
+					src = cms.InputTag('ca8PFJetsCHS'),
+					cut = cms.string('pt > 25 && abs(eta) < 2.4'))
+
+process.ca8Njettiness = Njettiness.clone(src = 'ca8PFJetsCHS')
+process.selectedca8Njettiness = Njettiness.clone(src = 'selectedca8PFJetsCHS')
+
+process.extraJetCols = cms.Sequence(
+	process.chs *
+	process.ca8PFJetsCHS *
+	process.ca8PFJetsCHSPruned *
+	process.ca8Njettiness *
+	process.cmsTopTagPFJetsCHS *
+	process.caTopTagInfos *
+	process.selectedca8PFJetsCHS *
+	process.selectedca8Njettiness
+)
+
 process.b2g = cms.EDFilter('b2g_miniAodAnalyzer_general',
 					vertexToken	= cms.InputTag('offlineSlimmedPrimaryVertices'),
 	    				metToken 	= cms.InputTag("slimmedMETs"),
@@ -32,20 +88,10 @@ process.b2g = cms.EDFilter('b2g_miniAodAnalyzer_general',
 					ca8prunedToken 	= cms.InputTag("ca8PFJetsCHSPruned"),
 					ca8subjetsToken	= cms.InputTag("ca8PFJetsCHSPruned", "SubJets"),
 					ca8TopTagToken 	= cms.InputTag("cmsTopTagPFJetsCHS"),
-					ca8TopTagInfoToken = cms.InputTag("caTopTagInfos"),
-				    )
+					ca8TopTagInfoToken = cms.InputTag("caTopTagInfos")
+)
 
-process.p = cms.Path(	
-			process.b2g
-		    )
-process.out = cms.OutputModule("PoolOutputModule",
-		   fileName = cms.untracked.string("ntuple.root"),
-		   SelectEvents   = cms.untracked.PSet( SelectEvents = cms.vstring('p')),
-		   outputCommands = cms.untracked.vstring('drop *','keep *_b2g*_*_*'))
-process.outpath = cms.EndPath(process.out)
-process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
-
-process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 10
-
-process.out.dropMetaData = cms.untracked.string("DROPPED")
+process.p = cms.Path(
+	process.extraJetCols *
+	process.b2g
+)
